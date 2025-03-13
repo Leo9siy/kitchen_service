29e0d9ecef69269ce0768d6aa054411c46c79de4
Leo <leonsiypopov@gmail.com>
2025-03-13 04:18:58 +0200

    Initial Kitchen app for Base Work

--- kitchen_app/admin.py
+++ kitchen_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from kitchen_app import models
+
 # Register your models here.
+
+
--- kitchen_app/models.py
+++ kitchen_app/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+
+class CookModel(AbstractUser):
+    year_of_experience = models.IntegerField(default=0)
+    username = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+
+class DishModel(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    ingredients = models.ManyToManyField(Ingredient, related_name='dish_ingredients')
+    cooks = models.ManyToManyField(CookModel, related_name='dishes')
+
+    def __str__(self):
+        return self.name
