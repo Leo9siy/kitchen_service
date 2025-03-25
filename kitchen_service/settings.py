a8aee1e4b88a003d46aeb86c94af0e09fbb7d214
Leo <leonsiypopov@gmail.com>
2025-03-25 17:54:09 +0200

    Protected secret key from others users

--- kitchen_service/settings.py
+++ kitchen_service/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ("django-insecure-v@j3cmwy"
-              "k8+&l1pgatm4(@sy7j+ez*ll+m@io=ex7d%@@+oj@)")
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,8 +41,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.admin",
-    "crispy_forms",
-    "crispy_bootstrap5",
     "kitchen_app",
 ]
 
